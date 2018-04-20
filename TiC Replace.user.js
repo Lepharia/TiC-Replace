c1ba1f185c4eece1ca0dd93b829920e812112950
Lepharia <38431158+Lepharia@users.noreply.github.com>
2018-04-20 16:13:20 +0200

    Changed version number format for tampermonkey

--- TiC Replace.user.js
+++ TiC Replace.user.js
@@ -2,7 +2,7 @@
 // @name     		TiC Replace
 // @namespace 	    tag:dennis.bonnmann@materna.de
 // @author			Dennis Bonnmann (dennis.bonnmann@materna.de)
-// @version         0.4.2
+// @version         0.5.1
 // @include			https://myintranet.materna.de/tic/*
 // @include         https://extranet.materna.de/*/tic/*
 // @exclude			https://myintranet.materna.de/tic/report*
