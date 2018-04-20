732598bbaef5888f92861ecd20b44695a22647c9
Lepharia <38431158+Lepharia@users.noreply.github.com>
2018-04-20 16:38:55 +0200

    Improved formating
    Ironed out a buncvh of formating irregularities that caused Brackets to show warnings. Pertaining mostly to mixed use of spaces and tabs and missing/unexpected spaces.

--- TiC Replace.user.js
+++ TiC Replace.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name     		TiC Replace
-// @namespace 	    tag:dennis.bonnmann@materna.de
-// @author			Dennis Bonnmann (dennis.bonnmann@materna.de)
+// @name            TiC Replace
+// @namespace       tag:dennis.bonnmann@materna.de
+// @author          Dennis Bonnmann (dennis.bonnmann@materna.de)
 // @version         0.5.1
-// @include			https://myintranet.materna.de/tic/*
+// @include         https://myintranet.materna.de/tic/*
 // @include         https://extranet.materna.de/*/tic/*
-// @exclude			https://myintranet.materna.de/tic/report*
+// @exclude         https://myintranet.materna.de/tic/report*
 // @grant           GM_addStyle
 // ==/UserScript==
 
@@ -14,22 +14,22 @@
 /*--- Create a button in a container div.  It will be styled and
     positioned with CSS.
 */
-var zNode       = document.createElement ('div');
+var zNode       = document.createElement('div');
 zNode.innerHTML =
-    '<div class="caption">'+
-        '- TiC Replace -'+
-    '</div>'+
-    '<div id="bt_Strip" class="tm_button">'+
-        'Sonderzeichen entfernen<br>(STRG+ALT+S)'+
-    '</div>'+
-    '<div id="bt_Replace" class="tm_button">'+
-        'Abkürzungen auflösen<br>(STRG+ALT+R)'+
+    '<div class="caption">' +
+        '- TiC Replace -' +
+    '</div>' +
+    '<div id="bt_Strip" class="tm_button">' +
+        'Sonderzeichen entfernen<br>(STRG+ALT+S)' +
+    '</div>' +
+    '<div id="bt_Replace" class="tm_button">' +
+        'Abkürzungen auflösen<br>(STRG+ALT+R)' +
     '</div>';
-zNode.setAttribute ('id', 'div_TicReplace');
-document.body.appendChild (zNode);
+zNode.setAttribute('id', 'div_TicReplace');
+document.body.appendChild(zNode);
 
 //--- Style our newly added elements using CSS.
-GM_addStyle ( multilineStr ( function () {/*!
+GM_addStyle(multilineStr(function () {/*!
     #div_TicReplace {
         position:               absolute;
         top:                    188px;
@@ -65,42 +65,46 @@ GM_addStyle ( multilineStr ( function () {/*!
     div.caption{
         margin-bottom:          25px;
     }
-*/} ) );
+*/
+}));
 
-function multilineStr (dummyFunc) {
-    var str = dummyFunc.toString ();
-    str     = str.replace (/^[^\/]+\/\*!?/, '') // Strip function () { /*!
-            .replace (/\s*\*\/\s*\}\s*$/, '')   // Strip */ }
-            .replace (/\/\/.+$/gm, '') // Double-slash comments wreck CSS. Strip them.
-            ;
+function multilineStr(dummyFunc) {
+    var str = dummyFunc.toString();
+    str     = str.replace(/^[^\/]+\/\*!?/, '') // Strip function () { /*!
+            .replace(/\s*\*\/\s*\}\s*$/, '')   // Strip */ }
+            .replace(/\/\/.+$/gm, ''); // Double-slash comments wreck CSS. Strip them.
     return str;
 }
 
 /**Handler for button bt_Replace**/
-document.getElementById ("bt_Replace").addEventListener (
-    "click", bt_replaceClick, false
+document.getElementById("bt_Replace").addEventListener(
+    "click",
+    bt_replaceClick,
+    false
 );
 
 /**Handler for button bt_Strip**/
-document.getElementById ("bt_Strip").addEventListener (
-    "click", bt_stripClick, false
+document.getElementById("bt_Strip").addEventListener(
+    "click",
+    bt_stripClick,
+    false
 );
 
 /**Define handler for Hotkeys**/
 document.addEventListener('keyup', doc_keyUp, false);
 
 /**Implements the handler for hotkeys. Stripping is triggered by CTRL+ALT+S, Replace by CTRL+ALT+R**/
-function doc_keyUp(e){
+function doc_keyUp(e) {
     
     // CTRL + ALT + S
-    if(e.ctrlKey && e.altKey && (e.keyCode == 83)){
-         bt_stripClick(null);       
+    if (e.ctrlKey && e.altKey && (e.keyCode == 83)) {
+        bt_stripClick(null);
     }
     
     //CTRL + ALT + R
-    if(e.ctrlKey && e.altKey && (e.keyCode == 82)){
+    if (e.ctrlKey && e.altKey && (e.keyCode == 82)) {
         bt_replaceClick(null);
-    }   
+    }
 }
 
 /**
@@ -109,57 +113,58 @@ function doc_keyUp(e){
  * @param originalString - the String to be replaced
  * @param replaceString - the replacement String
  */
-function tr_replace(originalString, replaceString){
-    var textNodes = document.evaluate("//TEXTAREA",document,null,XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,null);
+function tr_replace(originalString, replaceString) {
+    var textNodes = document.evaluate("//TEXTAREA", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
     var length = textNodes.snapshotLength;
     var searchRE = new RegExp(originalString, 'gi');
-    for (var i=0; i < textNodes.snapshotLength; i++){
-         var node = textNodes.snapshotItem(i);
-             node.value = node.value.replace(searchRE, replaceString);
+    var i;
+    for (i = 0; i < textNodes.snapshotLength; i + 1) {
+        var node = textNodes.snapshotItem(i);
+        node.value = node.value.replace(searchRE, replaceString);
     }
 }
 
 /**
  * Function that is called when bt_replace is clicked. Calls many instances of tr_replace to resolve shorthand for school subjects, business lines and other things
  */
-function bt_replaceClick(zEvent){
+function bt_replaceClick(zEvent) {
     /*******************\
      * Routineaufgaben *
     \*******************/
-    tr_replace(/\\Ass/,"Erfassen von Asset");
-    tr_replace(/\\Inc/,"Bearbeitung von Ticket");
+    tr_replace(/\\Ass/, "Erfassen von Asset");
+    tr_replace(/\\Inc/, "Bearbeitung von Ticket");
     /***************\
      * Schulfächer *
     \***************/
-    tr_replace(/\\E/,  "Englisch");
-    tr_replace(/\\ITS/,"IT-Systeme");
-    tr_replace(/\\WG/, "Wirtschaft und Geschäftsprozesse");
-    tr_replace(/\\PGL/,"Politik und Gesellschaftslehre");
-    tr_replace(/\\DK/, "Deutsch\/Kommunikation");
-    tr_replace(/\\AE/, "IT-Anwendungsentwicklung");
+    tr_replace(/\\E/,   "Englisch");
+    tr_replace(/\\ITS/, "IT-Systeme");
+    tr_replace(/\\WG/,  "Wirtschaft und Geschäftsprozesse");
+    tr_replace(/\\PGL/, "Politik und Gesellschaftslehre");
+    tr_replace(/\\DK/,  "Deutsch\/Kommunikation");
+    tr_replace(/\\AE/,  "IT-Anwendungsentwicklung");
     /******************\
      * Business Lines *
     \******************/
-    tr_replace(/\\COM/,"BL Communications");
-    tr_replace(/\\DE/, "BL Digital Enterprise");
-    tr_replace(/\\GOV/,"BL Government");
-    tr_replace(/\\ITF/,"BL IT-Factory");
-    tr_replace(/\\MOB/,"BL Mobility");
+    tr_replace(/\\COM/, "BL Communications");
+    tr_replace(/\\DE/,  "BL Digital Enterprise");
+    tr_replace(/\\GOV/,  "BL Government");
+    tr_replace(/\\ITF/, "BL IT-Factory");
+    tr_replace(/\\MOB/, "BL Mobility");
     /**********************\
      * Zentrale Einheiten *
     \**********************/
-    tr_replace(/\\OPS/,"ZE MS Operations");
+    tr_replace(/\\OPS/, "ZE MS Operations");
     /**********************\
      * Andere Abkürzungen *
     \**********************/
-    tr_replace(/\\GST/,"Geschäftsstelle");
-    tr_replace(/\\BS/, "Berufsschule");
+    tr_replace(/\\GST/, "Geschäftsstelle");
+    tr_replace(/\\BS/,  "Berufsschule");
 }
 
 /**
  * Function that is called when bt_strip is clicked. Removes special caracters used in my RegEx in TiC2Wb
  */
-function bt_stripClick(zEvent){
+function bt_stripClick(zEvent) {
     tr_replace(/\*/, "");
     tr_replace(/\_/, "");
     tr_replace(/\#/, "");
