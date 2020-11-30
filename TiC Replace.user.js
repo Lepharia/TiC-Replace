008519bf5eb6c8660cb880892f293a7037e804f7
Dennis Bonnmann <dennis.bonnmann@materna.de>
2020-11-30 10:47:02 +0100

    Version increment

--- TiC Replace.user.js
+++ TiC Replace.user.js
@@ -2,7 +2,7 @@
 // @name            TiC Replace_beta
 // @namespace       tag:dennis.bonnmann@materna.de
 // @author          Dennis Bonnmann (dennis.bonnmann@materna.de)
-// @version         0.8.3
+// @version         0.8.4
 // @include         https://myintranet.materna.de/tic/*
 // @include         https://extranet.materna.de/*/tic/*
 // @exclude         https://myintranet.materna.de/tic/report*
